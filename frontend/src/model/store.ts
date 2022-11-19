8a54892ee97816dc4e97724aba6ae90e5972e6b9
Wlad Meixner <hi@iamwlad.com>
2022-11-19 18:03:49 +0100

    feat: layout improvements

--- frontend/src/model/store.ts
+++ frontend/src/model/store.ts
@@ -2,17 +2,24 @@ import vanillaCreate from "zustand/vanilla";
 import create from "zustand";
 import { StonksServiceClient } from "../services/stonk-client";
 import { getClient } from "../services/transport";
-import { router } from "../router/router";
+import { StonkInfo, User } from "../services/vo-stonks";
 
 export type StonksState = {
   loading: boolean;
   // set after successfull regoster/newUser
   // removed after lobby closed or when 401 returned from any service call
   username?: string;
+  sessionUsers?: User[];
+
+  stonkInfos?: StonkInfo[];
 };
 
 export type StonksModifiers = {
   register: (name: string) => ReturnType<StonksServiceClient["newUser"]>;
+
+  getStonkInfo: (
+    stonkName: string
+  ) => ReturnType<StonksServiceClient["getStonkInfo"]>;
 };
 
 export const vanillaStore = vanillaCreate<StonksState & StonksModifiers>(
@@ -27,6 +34,7 @@ export const vanillaStore = vanillaCreate<StonksState & StonksModifiers>(
 
     return {
       username: undefined,
+      stockDetails: [],
       loading: false,
 
       register: (name: string) => {
@@ -38,6 +46,10 @@ export const vanillaStore = vanillaCreate<StonksState & StonksModifiers>(
           return resp;
         });
         // FIXME: handle server error
+      },
+
+      getStonkInfo: (stonk: string) => {
+        return withLoading(client.getStonkInfo(stonk));
       },
     };
   }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'background': '#B9FFF8',
-        'foreground': '#6FEDD6',
-        'accent': '#FF9551',
-        'primary': '#26263b',
-        'secondary': '#FFFFFF',
+      colors: {
+        background: "#5F43D0",
+        foreground: "#F4F4F4",
+        accent: "#FF9551",
+        primary: "#EEE",
+        secondary: "#EFEFEF",
       },
+      boxShadow: {},
     },
   },
   plugins: [],
-}
+};
