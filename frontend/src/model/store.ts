16924993d50474e98655a00c0d3399b6d0aa05fc
Wlad Meixner <hi@iamwlad.com>
2022-11-19 15:14:12 +0100

    wip: initial zustand state

---
+++ frontend/src/model/store.ts
@@ -0,0 +1,14 @@
+import vanillaCreate from "zustand/vanilla";
+import create from "zustand";
+import { StonksClient } from "../services/client";
+import { getClient } from "../services/transport";
+
+export const vanillaStore = vanillaCreate((set, get) => {
+  const client = getClient(StonksClient);
+
+  client.hello("test").then((resp) => console.log(resp));
+
+  return {};
+});
+
+export const useMainStore = create(vanillaStore);
