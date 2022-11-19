3389955a00e28d806130e0f1046fb81dba95c402
Wlad Meixner <hi@iamwlad.com>
2022-11-19 14:53:50 +0100

    remove protobuf

--- scripts/gen-proto.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-
-# generate frontend protos
-protoc --plugin=./frontend/node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./frontend/src/proto ./proto/messages.proto
-
-# generate backend protos
-protoc --go_out=./backend/proto --go_opt=paths=source_relative \
-    --go-grpc_out=./backend/proto --go-grpc_opt=paths=source_relative \
-    proto/messages.proto
---
+++ scripts/gen.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+
+gotsrpc ./backend/ıgotsrpc.yaml
