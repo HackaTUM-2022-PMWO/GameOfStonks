ab60eee3f9c54fc6203069169e27852de8bea9a5
Oliver Klukas <oliverklukas@t-online.de>
2022-11-19 16:08:31 +0100

    Add headlines

--- frontend/src/views/Onboard.tsx
+++ frontend/src/views/Onboard.tsx
@@ -12,14 +12,13 @@ function Onboard() {
     }
 
     return (
-        <div className="flex h-screen">
-            <form className="grid grid-cols-1 m-auto justify-center justify-items-center space-y-5">
-                <h1>Welcome to the GAME OF STONKS!</h1>
-                <h2>Tell us your name...</h2>
-                <Input type={"text"} value={userName} onChange={e => setUserName((e.target as unknown as HTMLTextAreaElement).value)}/>
-                <RouterButton onClick={handleSubmit} route={Routes.Lobby} text={"start trading stonks"}/>
-            </form>
-        </div>
+        <form className="flex flex-col justify-center items-center h-screen space-y-5">
+            <h1>Welcome to the GAME OF STONKS!</h1>
+            <h2>Tell us your name...</h2>
+            <Input type={"text"} value={userName}
+                   onChange={e => setUserName((e.target as unknown as HTMLTextAreaElement).value)}/>
+            <RouterButton onClick={handleSubmit} route={Routes.Lobby} text={"start trading stonks"}/>
+        </form>
     );
 }
 
--- frontend/src/views/StartStocks.tsx
+++ frontend/src/views/StartStocks.tsx
@@ -1,7 +1,11 @@
 function StartStocks() {
+    const item = require('./../assets/paperclip.png') // TODO: replace with dynamic icon image
+
     return (
-        <div>
-            StartStocks
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1>Your first STONKS:</h1>
+            <img className="w-32 h-32" src={item} alt={"item"}/>
+            <p>5x Paperclip (PACP)</p>
         </div>
     );
 }
