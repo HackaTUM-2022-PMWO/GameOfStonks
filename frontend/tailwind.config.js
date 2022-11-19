3c8db6b298a4bf5dd533335b0e1a78854fb4584c
Oliver Klukas <oliverklukas@t-online.de>
2022-11-19 11:56:30 +0100

    Update color palette and folder strucutre fronted

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
+    colors:{
+      'mint': '#B9FFF8',
+      'teal': '#6FEDD6',
+      'orange': '#FF9551',
+      'red': '#FF4A4A',
+      'white': '#FFFFFF',
+      'black': '#000000',
+    },
     extend: {},
   },
   plugins: [],
