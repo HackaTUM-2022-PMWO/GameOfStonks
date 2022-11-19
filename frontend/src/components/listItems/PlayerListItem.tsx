bd7681996780099f9f22fb4a015f8887a8270454
Oliver Klukas <oliverklukas@t-online.de>
2022-11-19 20:50:05 +0100

    lobby improvements

--- frontend/src/components/listItems/PlayerListItem.tsx
+++ frontend/src/components/listItems/PlayerListItem.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-export function PlayerListItem (props: {key:  React.Key | null | undefined, value: string}) {
-    const avatar = require('./../../assets/avatar.jpg') // TODO: replace with player images
-
+export function PlayerListItem (props: {key:  React.Key | null | undefined, idx: number, value: string}) {
+    const avatar = require(`../../assets/avatar/${props.idx}.jpg`)
+    const colors = ["#FCE700", "#EA047E", "#FF6D28", "#00F5FF", "#EB6440"]
     return(
         <li className="flex flex-row space-x-4 content-center" key={props.key}>
-            <img className="p-1 w-10 h-10 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+            <img style={{}} className={`p-1 w-10 h-10 rounded-full ring-2 ring-gray-[${colors[props.idx % 5]}]`}
                  src={avatar} alt="avatar"/>
             <p>{props.value}</p>
         </li>
-
     )
 }
