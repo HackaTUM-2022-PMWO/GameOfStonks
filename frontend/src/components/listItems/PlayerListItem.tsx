d019f2f0738e24544e174d51d2e23ae8dd99280e
Oliver Klukas <oliverklukas@t-online.de>
2022-11-19 21:23:20 +0100

    Made Lobby nicer

--- frontend/src/components/listItems/PlayerListItem.tsx
+++ frontend/src/components/listItems/PlayerListItem.tsx
@@ -6,14 +6,12 @@ export function PlayerListItem(props: {
   value: string;
 }) {
   const avatar = require(`../../assets/avatar/${props.idx}.jpg`);
-  const colors = ["#FCE700", "#EA047E", "#FF6D28", "#00F5FF", "#EB6440"];
+  const colors = ["p-1 w-10 h-10 rounded-full ring-2 ring-[#FCE700]", "p-1 w-10 h-10 rounded-full ring-2 ring-[#EA047E]", "p-1 w-10 h-10 rounded-full ring-2 ring-[#FF6D28]", "p-1 w-10 h-10 rounded-full ring-2 ring-[#00F5FF]", "p-1 w-10 h-10 rounded-full ring-2 ring-[#EB6440]"];
+
   return (
     <li className="flex flex-row items-center space-x-4 content-center">
       <img
-        style={{}}
-        className={`p-1 w-10 h-10 rounded-full ring-2 ring-gray-[${
-          colors[props.idx % 5]
-        }]`}
+        className={colors[props.idx]}
         src={avatar}
         alt="avatar"
       />
--- frontend/src/views/Lobby.tsx
+++ frontend/src/views/Lobby.tsx
@@ -3,11 +3,11 @@ import {Spinner} from "../components/spinner/Spinner";
 import {useStonkState} from "../model/store";
 
 function Lobby() {
-    const players = useStonkState((state) => state.sessionUsers);
+    const players = useStonkState((state) => state.sessionUsers); // TOOD: replace with SSE or getPlayers method
     return (
         <div className="flex flex-col justify-center items-center h-screen space-y-5">
-            <h1>Waiting for more STONK traders to join...</h1>
-            {players != null && <ul>
+            <h2>Waiting for more STONK traders to join...</h2>
+            {players != null && <ul className="space-y-3">
                 {players.map((player, index) => (
                     <PlayerListItem key={player.Name} idx={index} value={player.Name}/>
                 ))}
