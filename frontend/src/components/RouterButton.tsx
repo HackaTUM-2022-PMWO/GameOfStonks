069a069cd831f2ce3cd31a980c008dd3898d214e
Oliver Klukas <oliverklukas@t-online.de>
2022-11-19 14:53:20 +0100

    Basic Onboarding screen

--- frontend/src/components/RouterButton.tsx
+++
@@ -1,8 +0,0 @@
-import {Routes} from "../router/router";
-import {Link} from "react-router-dom";
-
-export function RouterButton(props: {route: Routes, text: string}){
-    return (
-        <Link className="flex bg-teal text-white font-medium hover:font-bold ease-in duration-200 drop-shadow py-2 px-4 rounded max-w-fit" to={props.route}>{props.text}</Link>
-    )
-}
--- frontend/src/views/Onboard.tsx
+++ frontend/src/views/Onboard.tsx
@@ -1,8 +1,22 @@
+import {RouterButton} from "../components/buttons/RouterButton";
+import {Routes} from "../router/router";
+import {useState} from "react";
+import {Input} from "../components/inputs/Input";
+
 function Onboard() {
+    const [userName, setUserName] = useState('');
+
+    const handleSubmit = () => {
+        // alert(`The name you entered was: ${userName}`);
+        // TODO: handle errors
+    }
+
     return (
-        <div>
-            Onboard
-        </div>
+            <form className="grid grid-cols-1 justify-center justify-items-center space-y-5">
+                <label>Tell us your name...</label>
+                <Input type={"text"} value={userName} onChange={e => setUserName((e.target as unknown as HTMLTextAreaElement).value)}/>
+                <RouterButton onClick={handleSubmit} route={Routes.Lobby} text={"start trading stonks"}/>
+            </form>
     );
 }
 
