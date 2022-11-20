0476151eb2edacd964804da1880a1a7be9412105
philippwulff <philipp.sylt@gmail.com>
2022-11-20 06:04:18 +0100

    added bot

---
+++ backend/bot/bot.go
@@ -0,0 +1,28 @@
+package bot
+
+import (
+	"github.com/hackaTUM/GameOfStonks/store"
+)
+
+type Bot interface {
+	Execute(map[string]float64)
+}
+
+type RandomBot struct {
+	randSellProb 		float64
+	randBuyProb 		float64
+	outsideMarketPrice	float64
+	orderP       		store.OrderPersistor
+}
+
+func newRandomBot(randSellProb, randBuyProb, outsideMarketPrice float64) *RandomBot {
+	return &RandomBot{
+		randSellProb: randSellProb,
+		randBuyProb: randBuyProb,
+		outsideMarketPrice: outsideMarketPrice,
+	}
+}
+
+func (b *RandomBot) Execute(stockPrices map[string]float64) {
+	
+}
